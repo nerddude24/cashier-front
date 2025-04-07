b609f77e6c76fdb8625fa9165523cfa1949947c3
Sidali Djeghbal <sidalidjeghbal@gmail.com>
2025-04-07 22:07:03 +0100

    refactor: Clean up Receipt component layout

--- src/components/Order/Receipt.tsx
+++ src/components/Order/Receipt.tsx
@@ -26,10 +26,10 @@ export default function Receipt({
 
 
   return (
-    <div className="fixed inset-0 bg-black/20 flex items-center justify-center p-6 print:p-0 print:bg-[#232323] z-100">
-      <div className=" bg-[#1a1a1a] rounded-lg w-full max-w-2xl p-8 print:p-4 print:max-w-none">
-        <div className="flex justify-between items-start mb-6 print:mb-4">
-          <div className="relative w-[200px] h-[120px]">
+    <div className="fixed inset-0 bg-black/20 flex items-center justify-center p-6 z-100">
+      <div className=" bg-[#1a1a1a] rounded-lg w-full max-w-2xl p-8">
+        <div className="flex justify-between items-start">
+          <div className="relative left-44 w-[220px] h-[140px]">
             <Image
               src="/ShoppingCenter.svg"
               alt="ShoppingCenter Logo"
@@ -43,7 +43,7 @@ export default function Receipt({
           </div>
         </div>
 
-        <div className="mb-6 print:mb-4">
+        <div className="mb-6">
           <div className="grid grid-cols-6 p-2 bg-[#232323] text-sm mb-2 text-left items-center gap-2 font-semibold">
             <div>N</div>
             <div>PRODUCT NAME</div>
@@ -67,16 +67,16 @@ export default function Receipt({
           ))}
         </div>
 
-        <div className="flex justify-between items-baseline mb-8 print:mb-4 pt-4">
+        <div className="flex justify-between items-baseline mb-8 pt-4">
           <div className=" text-lg">TOTAL</div>
           <div className="text-4xl font-mono">${total.toFixed(2)}</div>
         </div>
-        <h1 className="text-center text-l font-semibold mb-4 print:mb-2">
+        <h1 className="text-center text-l font-semibold mb-4">
           <span className="text-red-500">IMPORTANT:</span> If you want to make a
           return, please keep this receipt.
         </h1>
 
-        <div className="flex justify-center gap-4 print:hidden">
+        <div className="flex justify-center gap-4 ">
           <button
             onClick={onClose}
             className="px-4 py-2 border hover:bg-white/10 cursor-pointer transition-colors"
